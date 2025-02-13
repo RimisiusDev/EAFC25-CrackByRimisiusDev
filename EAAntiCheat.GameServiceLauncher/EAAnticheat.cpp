d59bb460dae71f859a4a892c6e3fe32c612f010d
Rikko Matsumato <kuttimsx@hotmail.com>
2025-02-13 19:52:18 +0200

    Update EAAnticheat.cpp

--- EAAntiCheat.GameServiceLauncher/EAAnticheat.cpp
+++ EAAntiCheat.GameServiceLauncher/EAAnticheat.cpp
@@ -10,12 +10,12 @@ int main()
 	startup.cb = sizeof(startup);
 	ZeroMemory(&pi, sizeof(pi));
 	SetConsoleTitleA("EAAntiCheat.GameServiceLauncher(FAKE) by RikkoMatsumatoOfficial");
-	if (!CreateProcessA("EAFC25.exe", (LPSTR)0, NULL, NULL, NULL, 0, NULL, NULL, &startup, &pi)) {
-		MessageBoxA(0, "Not Founded EAFC25.exe", "EAAntiCheat.GameServiceLauncher", MB_OK | MB_ICONERROR);
+	if (!CreateProcessA("FC25.exe", (LPSTR)0, NULL, NULL, NULL, 0, NULL, NULL, &startup, &pi)) {
+		MessageBoxA(0, "Not Founded FC25.exe", "EAAntiCheat.GameServiceLauncher", MB_OK | MB_ICONERROR);
 		exit(443);
 	}
 	else {
-		printf("Successfully Founded EAFC25.exe and Launching...");
+		printf("Successfully Founded FC25.exe and Launching...");
 		CloseHandle(pi.hProcess);
 		CloseHandle(pi.hThread);
 		exit(344);
